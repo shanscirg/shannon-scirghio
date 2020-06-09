c998b1a1a6aaa2bcd156539cbed299c56929760b
Shannon Scirghio <shanscirg7@gmail.com>
2020-06-08 18:38:29 -0700

    created navbar, set up index.html, index.js, and app.js files

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+
+export default function NavBar() {
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                <Navbar.Brand className="brand" href="/About">Shannon Scirghio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Link to="/About" className="navbartext">About</Link>
+                        <Link to="/Projects" className="navbartext">Projects</Link>
+                        <Link to="/Contact" className="navbartext">Contact</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
