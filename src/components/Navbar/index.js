7f1d359ae51532deba5ae62e6e0376e00417b556
Shannon Scirghio <shanscirg7@gmail.com>
2020-06-08 22:43:01 -0700

    navbar styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,17 +8,17 @@ export default function NavBar() {
 
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="light">
-                <Navbar.Brand className="brand" href="/About">
-                    <h3 className="navhead">Shannon Scirghio</h3>
-                    <h5 className="navhead">Full Stack Web Developer</h5>
+            <Navbar collapseOnSelect expand='lg' bg='light' className='navnav'>
+                <Navbar.Brand className='brand' href='/About'>
+                    <h3 className='navhead' id='namehead'>Shannon Scirghio</h3>
+                    <h5 className='navhead'>Full Stack Web Developer</h5>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Link to="/About" className="navbartext">About</Link>
-                        <Link to="/Projects" className="navbartext">Projects</Link>
-                        <Link to="/Contact" className="navbartext">Contact</Link>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav className='ml-auto'>
+                        <Link to='/About' className='navbartext'>About</Link>
+                        <Link to='/Projects' className='navbartext'>Projects</Link>
+                        <Link to='/Contact' className='navbartext'>Contact</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
