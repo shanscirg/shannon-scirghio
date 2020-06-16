cfa4de7d3d927bc9e07db20c9757965c9dcb2884
Shannon Scirghio <shanscirg7@gmail.com>
2020-06-15 22:17:35 -0700

    created and styled project card component

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Col, Image } from 'react-bootstrap';
+import githubLogo from '../../utils/images/github.png';
+
+
+
+export default function ProjectCard({ imageSrc, siteName, liveSite, description, githubSite, siteLogo }) {
+    return (
+        <Col className='project-card' style={{border: "1px solid black", padding: "20px", margin: "30px", "box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", background: "rgba(0, 0, 0, 0.3)"}} xs={12} lg={5}>
+            <div className='image'>
+                <a target="_blank" href={liveSite}>
+                    <Image src={imageSrc} alt={siteName} style={{ height: "30%", width: "100%" }}/>
+                </a>
+            </div>
+            <div className='description' style={{color: "white"}}>
+                {description}
+            </div>
+            <div className='links'>
+                <a target="_blank" href={githubSite} style={{'margin-right': '10px'}}>
+                    <Image src={githubLogo} style={{ height: "20px", width: "20px" }}/>
+                    GitHub
+                </a>
+                <a target="_blank" href={liveSite}>
+                    <Image src={siteLogo} style={{ height: "20px", width: "20px" }}/>
+                    {siteName}
+                </a>
+            </div>
+        </Col>
+    )
+}
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,6 +1,12 @@
 import React, { useContext, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import { MyContext } from '../../utils/Context';
+import ProjectCard from '../../components/ProjectCard';
+import slangSS from '../../utils/images/slangSS1.png';
+import slangFavicon from '../../utils/images/slangFavicon.png';
+import placeholder from '../../utils/images/placeholder.png';
+import whatsSS from '../../utils/images/whatsSS.png';
+import whatsFav from '../../utils/images/whatsFav.png';
 
 export default function Projects() {
 
@@ -10,12 +16,39 @@ export default function Projects() {
         isMenuOpen && toggleMenu();
     }, []);
 
+    const slangHref = "https://slanguageapp.herokuapp.com"
+    const slangDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    const slangGithub = 'https://github.com/cha2169540/Slanguage';
+
+    const whatsHref = 'https://whats-my-life.herokuapp.com/';
+    const whatsDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    const whatsGithub = 'https://github.com/shanscirg/whats-my-life'
+
     return (
-        <Container fluid="md">
-            <Row className='mt-5'>
-                <Col xs={12} lg={6}>
-                    Projects Here
-                </Col>
+        <Container fluid='md' className='home-container'>
+            <Row>
+                <ProjectCard
+                    imageSrc={slangSS}
+                    siteName={'Slanguage'}
+                    liveSite={slangHref}
+                    description={slangDescription}
+                    githubSite={slangGithub}
+                    siteLogo={slangFavicon}
+                />
+                <ProjectCard
+                    imageSrc={whatsSS}
+                    siteName={'What\'s My Life!?'}
+                    liveSite={whatsHref}
+                    description={whatsDescription}
+                    githubSite={whatsGithub}
+                    siteLogo={whatsFav}
+                />
+                <ProjectCard
+                    imageSrc={placeholder}
+                    siteName={'idkyet'}
+                    liveSite={whatsHref}
+                    description={slangDescription}
+                />
             </Row>
         </Container>
     )
