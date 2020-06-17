050282e82a36e3eebc9e8abe454a5349c29035a1
Shannon Scirghio <shanscirg7@gmail.com>
2020-06-17 16:45:39 -0700

    added refs to a tags

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -8,16 +8,16 @@ export default function ProjectCard({ imageSrc, siteName, liveSite, description,
     return (
         <Col className='project-card' style={{height:'100%', overflow: 'auto', border: '1px solid black', padding: '20px', margin: '30px', 'box-shadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', background: 'rgba(0, 0, 0, 0.3)'}} xs={12} lg={5}>
             <div className='image'>
-                <a target='_blank' href={liveSite}>
+                <a target='_blank' rel="noopener noreferrer" href={liveSite}>
                     <Image src={imageSrc} alt={siteName} style={{ height: '30%', width: '100%' }}/>
                 </a>
             </div>
             <div className='links' style={{'margin-top': '10px'}}>
-                <a target='_blank' href={githubSite} style={{'margin-right': '15px'}}>
+                <a target='_blank' rel="noopener noreferrer" href={githubSite} style={{'margin-right': '15px'}}>
                     <Image src={githubLogo} style={{ height: '20px', width: '20px', 'margin-right': '3px' }}/>
                     GitHub
                 </a>
-                <a target='_blank' href={liveSite}>
+                <a target='_blank' rel="noopener noreferrer" href={liveSite}>
                     <Image src={siteLogo} style={{ height: '20px', width: '20px', 'margin-right': '3px' }}/>
                     {siteName}
                 </a>
