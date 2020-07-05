479fde862cf8e8fb61645e8b782639ae2b3ab947
Shannon Scirghio <shanscirg7@gmail.com>
2020-07-05 14:15:02 -0700

    changed about intro on mobile's animation/layout, changed projectCard component to have 'read more/less' dropdown

--- src/components/AboutIntro/index.js
+++ src/components/AboutIntro/index.js
@@ -5,13 +5,29 @@ import './style.css';
 export default function AboutIntro() {
 
     return (
-        <div className='intro'>
-            <Row className='mt-4'>
-                <h1 className='text-center mx-auto d-block about-intro'>Shannon Scirghio</h1>
-            </Row>
-            <Row className='mb-3'>
-                <h3 className='text-center mx-auto d-block about-intro words'>Developer | Innovator | Optimist</h3>
-            </Row>
-        </div>
+        <>
+            <div className='intro'>
+                <Row className='mt-4'>
+                    <h1 className='text-center mx-auto d-block about-intro'>Shannon Scirghio</h1>
+                </Row>
+                <Row className='mb-3'>
+                    <h3 className='text-center mx-auto d-block about-intro words'>Developer | Innovator | Optimist</h3>
+                </Row>
+            </div>
+            <div className='intro-mobile'>
+                <Row className='mt-4'>
+                    <h1 className='text-center mx-auto d-block about-intro'>Shannon Scirghio</h1>
+                </Row>
+                <Row className='slideIn1'>
+                    <h3 className='text-center mx-auto d-block about-intro words'>Developer</h3>
+                </Row>
+                <Row className='slideIn2'>
+                    <h3 className='text-center mx-auto d-block about-intro words'>Innovator</h3>
+                </Row>
+                <Row className='slideIn3'>
+                    <h3 className='text-center mx-auto d-block about-intro words'>Optimist</h3>
+                </Row>
+            </div>
+        </>
     )
 }
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import { Col, Image } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, Image, Collapse } from 'react-bootstrap';
 import githubLogo from '../../utils/images/github.png';
 
 
 
 export default function ProjectCard({ imageSrc, siteName, liveSite, description, role, techs, githubSite, siteLogo }) {
+
+    const [open, setOpen] = useState(false);
+
     return (
-        <Col className='project-card' style={{maxHeight:'640px', minHeight: '605px', overflow: 'auto', border: '1px solid black', padding: '20px', margin: '30px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', background: 'rgba(34, 49, 63, 0.4)'}} xs={12} lg={5}>
+        <Col className='project-card' style={{ minHeight: '420px', overflow: 'auto', border: '1px solid black', padding: '20px', margin: '30px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', background: 'rgba(34, 49, 63, 0.4)' }} xs={12} lg={5}>
             <div className='image'>
-                <a target='_blank' rel="noopener noreferrer" href={liveSite}>
-                    <Image src={imageSrc} alt={siteName} style={{ height: '30%', width: '100%' }}/>
+                <a target='_blank' rel='noopener noreferrer' href={liveSite}>
+                    <Image src={imageSrc} alt={siteName} style={{ height: '30%', width: '100%' }} />
                 </a>
             </div>
-            <div className='links' style={{marginTop: '10px'}}>
-                <a target='_blank' rel="noopener noreferrer" href={githubSite} style={{marginRight: '15px'}}>
-                    <Image src={githubLogo} style={{ height: '20px', width: '20px', marginRight: '3px' }}/>
+            <div className='links' style={{ marginTop: '10px' }}>
+                <a target='_blank' rel='noopener noreferrer' href={githubSite} style={{ marginRight: '15px' }}>
+                    <Image src={githubLogo} style={{ height: '20px', width: '20px', marginRight: '3px' }} />
                     GitHub
                 </a>
-                <a target='_blank' rel="noopener noreferrer" href={liveSite}>
-                    <Image src={siteLogo} style={{ height: '20px', width: '20px', marginRight: '3px' }}/>
+                <a target='_blank' rel='noopener noreferrer' href={liveSite}>
+                    <Image src={siteLogo} style={{ height: '20px', width: '20px', marginRight: '3px' }} />
                     {siteName}
                 </a>
             </div>
-            <div className='description' style={{color: 'white', marginTop: '10px'}}>
-                <strong><i>{siteName}</i></strong> {description}
+            <div className='description' style={{ color: 'white', marginTop: '10px' }}>
+                <strong><i>{siteName}</i></strong> {description}{!open && <span id='dots' style={{ color: 'white' }}>..</span>}
+
             </div>
-            <div className='role' style={{color: 'white', marginTop: '10px'}}>
-                <strong>My Role</strong>: {role}
-            </div>
-            <div className='techs' style={{color: 'white', marginTop: '10px'}}>
-                <strong>Techs used</strong>: {techs}
-            </div>
-        </Col>
+            <Collapse in={open}>
+                <div id='collapse-text'>
+                    <div className='role' style={{ color: 'white', marginTop: '10px' }}>
+                        <strong>My Role</strong>: {role}
+                    </div>
+                    <div className='techs' style={{ color: 'white', marginTop: '10px' }}>
+                        <strong>Techs used</strong>: {techs}
+                    </div>
+                </div>
+            </Collapse>
+            <button type='button' class='myBtn' style={{ padding: '3px 8px', fontSize: '14px', marginTop: '10px' }} onClick={() => setOpen(!open)} aria-controls='collapse-text'
+                aria-expanded={open}>
+                Read {open ? 'Less' : 'More'}
+            </button>
+        </Col >
     )
 }
