60bdaaf108856ab4d5e14b5528812a6e85f8fc0a
Shannon Scirghio <shanscirg7@gmail.com>
2020-09-01 12:12:06 -0700

    adding build folder

---
+++ build/precache-manifest.c08a6ff577658f0330e64e23681d96fb.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d2c7817f8165771f6aaacafaa87152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78b8a67ffc31d757bce4",
+    "url": "/static/css/main.742d5c1c.chunk.css"
+  },
+  {
+    "revision": "c6904248a53d42782a3a",
+    "url": "/static/js/2.ddbf632c.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.ddbf632c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b8a67ffc31d757bce4",
+    "url": "/static/js/main.b3ebf1b2.chunk.js"
+  },
+  {
+    "revision": "6699a56fa02b5eb181bd",
+    "url": "/static/js/runtime-main.4f418c66.js"
+  },
+  {
+    "revision": "300f3233c50bddd5afe429376a176c6a",
+    "url": "/static/media/Scirghio_FrontendSoftwareDev_Aug2020.300f3233.pdf"
+  },
+  {
+    "revision": "58bad5c84895c163279538be6665cb5b",
+    "url": "/static/media/a1SS.58bad5c8.png"
+  },
+  {
+    "revision": "79f9adf967a2b9fb30a34111c6ecda0c",
+    "url": "/static/media/background2.79f9adf9.jpeg"
+  },
+  {
+    "revision": "91eddd882da6ce4220b2a2e764f896f2",
+    "url": "/static/media/burgerSS.91eddd88.png"
+  },
+  {
+    "revision": "d388531001ebfc948cd57e060dbcac4b",
+    "url": "/static/media/employeeSS.d3885310.png"
+  },
+  {
+    "revision": "041461f7d31350718025215c8def9b1d",
+    "url": "/static/media/github.041461f7.png"
+  },
+  {
+    "revision": "5982edea8141be24bfb656469d47e12f",
+    "url": "/static/media/gmail.5982edea.png"
+  },
+  {
+    "revision": "c0b49a1c86abbd3a5ac45b361ce3ecf3",
+    "url": "/static/media/linkedin.c0b49a1c.png"
+  },
+  {
+    "revision": "05c571e8e4716fbe70b5b40ec262d956",
+    "url": "/static/media/moodiaSS.05c571e8.png"
+  },
+  {
+    "revision": "80e396e86d468d21da6b42a67d2871c4",
+    "url": "/static/media/resume.80e396e8.png"
+  },
+  {
+    "revision": "d935b76503f0581d333561ae8d4f2721",
+    "url": "/static/media/shan.d935b765.jpeg"
+  },
+  {
+    "revision": "ba9a040261004dd297abe8a9a1189022",
+    "url": "/static/media/shanTree2.ba9a0402.jpeg"
+  },
+  {
+    "revision": "d39a8b86b9b9d7ec43b41da737c3230d",
+    "url": "/static/media/shannon.d39a8b86.JPG"
+  },
+  {
+    "revision": "2c8835de430b3b9dd0796389bc8bad98",
+    "url": "/static/media/shannonAbout.2c8835de.JPG"
+  },
+  {
+    "revision": "510fdf56c8b7e30cfba8655eabefe0f1",
+    "url": "/static/media/slangSS2.510fdf56.png"
+  },
+  {
+    "revision": "d4f70030ef090fa296f897856eb82927",
+    "url": "/static/media/weatherSS.d4f70030.png"
+  },
+  {
+    "revision": "672d2e100e4e0c8dfab25972c5f0642b",
+    "url": "/static/media/whatsSS.672d2e10.png"
+  }
+]);
